895e112802340821da077f7e4505fef10e2d5b2e
kjordan-22 <113301700+kjordan-22@users.noreply.github.com>
2024-01-09 01:59:15 -0500

    mh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const targetDate = '2024-12-12T19:00:00';
 
-function getTimeSegmentElements(segmentElement){
+function getTimeSegmentElements(segmentElement) {
     const segmentDisplay = segmentElement.querySelector(
         '.segment-display'
     );
@@ -13,4 +13,29 @@ function getTimeSegmentElements(segmentElement){
     const segmentOverlay = segmentDisplay.querySelector(
         '.segment-overlay'
     );
+    const segmentOverlayTop = segmentOverlay.querySelector(
+        '.segment-overlay_top'
+    );
+    const segmentOverlayBottom = segmentOverlay.querySelector(
+        '.segment-overlay_bottom'
+    );
+
+    return{
+        segmentDisplayTop,
+        segmentDisplayBottom,
+        segmentOverlay,
+        segmentOverlayTop,
+        segmentOverlayBottom
+    }
+}
+
+function updateSegmentValues(displayElement, overlayElement, value){
+    displayElement.textContent = value;
+    overlayElement.textContent = value;
+}
+
+function updateTimeSegment(segmentElement, timeValue) {
+    const segmentElement =getTimeSegmentElements(segmentElement);
+
+    segmentElements.segmentOverlay
 }
