f279ab2b7a2b28368a3d43053059897a3bb6160b
kjordan-22 <113301700+kjordan-22@users.noreply.github.com>
2024-01-11 17:36:22 -0500

    cgfgbv

---
+++ card.js
@@ -0,0 +1,18 @@
+let tColorA = document.getElementById('tColorA'),
+tColorB = document.getElementById('tColorB'),
+tColorC = document.getElementById('tColorC'),
+iconA = document.querySelector('.fa-credit-card'),
+iconB = document.querySelector('.fa-building-columns'),
+iconC = document.querySelector('.fa-wallet'),
+cDetails = document.querySelector('.card-details');
+
+
+function doFun(){
+    tColorA.style.color = "greenyellow";
+    tColorB.style.color = "#444";
+    tColorC.style.color = "#444";
+    iconA.style.color = "greenyellow"
+    iconB.style.color = "#aaa";
+    iconC.style.color = "#aaa";
+    cDetails.style.display = "block";
+}
